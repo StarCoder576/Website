86e0d9bd768b835463f3b2cb28a442840ad427ba
StarCoder576 <157668972+StarCoder576@users.noreply.github.com>
2024-02-15 21:16:51 +0000

    update index.html and script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,27 @@
+// Variables ########################################################################################################################
+let chairmode = false;
+let tablemode = false;
+let stoolmode = false;
+let darkmode = false;
+let legcount = 4;
+
 //Back Button #############################################################################################################
 
 function back() {
-    allwoodoff()
-    alllegsoff()
-    woodselector.style.display = 'none'
-    chairselector.style.display = 'none'
-    home.style.display = 'inline-block'
-    backbutton.style.display = 'none'
+    allwoodoff();
+    alllegsoff();
+    woodselector.style.display = 'none';
+    chairselector.style.display = 'none';
+    chairselectorDARK.style.display = 'none';
+    tableselector.style.display = 'none';
+    tableselectorDARK.style.display = 'none';
+    stoolselectorDARK.style.display = 'none';
+    stoolselector.style.display = 'none';
+    home.style.display = 'inline-block';
+    backbutton.style.display = 'none';
+    chairmode = false;
+    tablemode = false;
+    stoomode = false;
 }
 //Home Area  #############################################################################################################
 
@@ -17,6 +32,7 @@ function chairarea() {
     chairselector.style.display = 'inline-block';
     backbutton.style.display = 'inline-block';
     home.style.display = 'none';
+    chairmode = true;
 }
 
 
@@ -26,8 +42,51 @@ function chairarea() {
 
 
 
+//Turns all Legs Off ################################################################################################
+function alllegsoff() {
 
+    //Light Mode Chair
+    oneleg.style.display= 'none';
+    twoleg.style.display= 'none';
+    threeleg.style.display= 'none';
+    fourleg.style.display= 'none';
+    fiveleg.style.display= 'none';
+    
+    //Dark Mode Chair
+    onelegDARK.style.display= 'none';
+    twolegDARK.style.display= 'none';
+    threelegDARK.style.display= 'none';
+    fourlegDARK.style.display= 'none';
+    fivelegDARK.style.display= 'none';
 
+    //Light Mode Table
+    tableoneleg.style.display= 'none';
+    tabletwoleg.style.display= 'none';
+    tablethreeleg.style.display= 'none';
+    tablefourleg.style.display= 'none';
+    tablefiveleg.style.display= 'none';
+
+    //Dark Mode Table
+    tableonelegDARK.style.display= 'none';
+    tabletwolegDARK.style.display= 'none';
+    tablethreelegDARK.style.display= 'none';
+    tablefourlegDARK.style.display= 'none';
+    tablefivelegDARK.style.display= 'none';
+
+    //Light Mode Stool
+    stooloneleg.style.display= 'none';
+    stooltwoleg.style.display= 'none';
+    stoolthreeleg.style.display= 'none';
+    stoolfourleg.style.display= 'none';
+    stoolfiveleg.style.display= 'none';
+
+    //Dark Mode Stool
+    stoolonelegDARK.style.display= 'none';
+    stooltwolegDARK.style.display= 'none';
+    stoolthreelegDARK.style.display= 'none';
+    stoolfourlegDARK.style.display= 'none';
+    stoolfivelegDARK.style.display= 'none';
+}
 
 
 
@@ -48,13 +107,13 @@ function allwoodoff() {
 }
 
 function oakS() {
-    allwoodoff()
-    oak.style.display = 'inline-block'
+    allwoodoff();
+    oak.style.display = 'inline-block';
 }
 
 function beechS() {
-    allwoodoff()
-    beech.style.display = 'inline-block'
+    allwoodoff();
+    beech.style.display = 'inline-block';
 }
 
 function acaciaS() {
@@ -73,14 +132,7 @@ function teekS() {
 }
 
 
-// CHAIR
-function alllegsoff() {
-    oneleg.style.display= 'none';
-    twoleg.style.display= 'none';
-    threeleg.style.display= 'none';
-    fourleg.style.display= 'none';
-    fiveleg.style.display= 'none';
-}
+// CHAIR ####################################################################################
 
 function leg1() {
     alllegsoff()
@@ -107,15 +159,133 @@ function leg5() {
     fiveleg.style.display= 'inline-block'
 }
 
-
-
-//TABLE
-function tablelegsoff() {
-    
+function leg1DARK() {
+    alllegsoff()
+    onelegDARK.style.display = 'inline-block'
 }
 
+function leg2DARK() {
+    alllegsoff()
+    twolegDARK.style.display = 'inline-block'
+}
 
+function leg3DARK() {
+    alllegsoff()
+    threelegDARK.style.display = 'inline-block'
+}
 
+function leg4DARK() {
+    alllegsoff()
+    fourlegDARK.style.display = 'inline-block'
+}
+
+function leg5DARK() {
+    alllegsoff()
+    fivelegDARK.style.display = 'inline-block'
+}
+
+//TABLE
+function tableleg1() {
+    alllegsoff()
+    tableoneleg.style.display = 'inline-block'
+}
+
+function tableleg2() {
+    alllegsoff()
+    tabletwoleg.style.display = 'inline-block'
+}
+
+function tableleg3() {
+    alllegsoff()
+    tablethreeleg.style.display = 'inline-block'
+}
+
+function tableleg4() {
+    alllegsoff()
+    tablefourleg.style.display = 'inline-block'
+}
+
+function tableleg5() {
+    alllegsoff()
+    tablefiveleg.style.display = 'inline-block'
+}
+
+function tableleg1DARK() {
+    alllegsoff()
+    tableonelegDARK.style.display = 'inline-block'
+}
+
+function tableleg2DARK() {
+    alllegsoff()
+    tabletwolegDARK.style.display = 'inline-block'
+}
+
+function tableleg3DARK() {
+    alllegsoff()
+    tablethreelegDARK.style.display = 'inline-block'
+}
+
+function tableleg4DARK() {
+    alllegsoff()
+    tablefourlegDARK.style.display = 'inline-block'
+}
+
+function tableleg5DARK() {
+    alllegsoff()
+    tablefivelegDARK.style.display = 'inline-block'
+}
+
+//Stool
+
+function stoolleg1 () {
+    alllegsoff()
+    stooloneleg.style.display = 'inline-block'
+}
+
+function stoolleg2 () {
+    alllegsoff()
+    stooltwoleg.style.display = 'inline-block'
+}
+
+function stoolleg3 () {
+    alllegsoff()
+    stoolthreeleg.style.display = 'inline-block'
+}
+
+function stoolleg4 () {
+    alllegsoff()
+    stoolfourleg.style.display = 'inline-block'
+}
+
+function stoolleg5 () {
+    alllegsoff()
+    stoolfiveleg.style.display = 'inline-block'
+}
+
+function stoolleg1DARK () {
+    alllegsoff()
+    stoolonelegDARK.style.display = 'inline-block'
+}
+
+function stoolleg2DARK () {
+    alllegsoff()
+    stooltwolegDARK.style.display = 'inline-block'
+}
+
+function stoolleg3DARK () {
+    alllegsoff()
+    stoolthreelegDARK.style.display = 'inline-block'
+}
+
+function stoolleg4DARK () {
+    alllegsoff()
+    stoolfourlegDARK.style.display = 'inline-block'
+}
+
+function stoolleg5DARK () {
+    alllegsoff()
+    stoolfivielegDARK.style.display = 'inline-block'
+}
 
 
 
